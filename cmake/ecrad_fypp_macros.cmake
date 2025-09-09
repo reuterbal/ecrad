da09350803b228052832d58b5d1f4781cdeaf796
Johan Ericsson <johan.ericsson@ecmwf.int>
2025-09-09 09:29:20 +0000

    add support for fypp CMake macro being called from multiple src dirs.

--- cmake/ecrad_fypp_macros.cmake
+++ cmake/ecrad_fypp_macros.cmake
@@ -8,24 +8,41 @@
 # nor does it submit to any jurisdiction.
 
 macro( ecrad_ifs_process_fypp fypp_file )
+
+    get_filename_component(input_dir ${fypp_file} DIRECTORY)
+    if(input_dir STREQUAL "")
+        # Check for file in current source dir if only a file name is given
+        set(fypp_file_dir ${CMAKE_CURRENT_SOURCE_DIR})
+        set(file_name ${fypp_file})
+        set(file_path ${CMAKE_CURRENT_SOURCE_DIR}/${fypp_file})
+    else()
+        # Assume full path is given
+        get_filename_component(file_path ${fypp_file} ABSOLUTE
+                              BASE_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+        get_filename_component(fypp_file_dir ${file_path} DIRECTORY)
+        get_filename_component(file_name ${file_path} NAME)
+    endif()
+    if (NOT EXISTS ${file_path}.fypp)
+        message (FATAL_ERROR "The fypp file: ${file_name}.fypp does not exist in the current source directory: ${fypp_file_dir}")
+    endif()
     # If FYPP has been enabled, regenerate the F90 source files otherwise simply link
     # or copy commited versions
     if( HAVE_FYPP )
         add_custom_command(
             OUTPUT
-                ${CMAKE_CURRENT_BINARY_DIR}/${fypp_file}.F90
+                ${CMAKE_CURRENT_BINARY_DIR}/${file_name}.F90
             COMMAND
-                ${FYPP} -m os -m field_config -M ${CMAKE_CURRENT_SOURCE_DIR}
-                ${CMAKE_CURRENT_SOURCE_DIR}/${fypp_file}.fypp
-                ${CMAKE_CURRENT_BINARY_DIR}/${fypp_file}.F90
+                ${FYPP} -m os -m field_config -M ${fypp_file_dir}
+                ${file_path}.fypp
+                ${file_path}.F90
             DEPENDS
-                ${CMAKE_CURRENT_SOURCE_DIR}/${fypp_file}.fypp
-                ${CMAKE_CURRENT_SOURCE_DIR}/radiation_fields_config.yaml
+                ${file_path}.fypp
+                ${fypp_file_dir}/radiation_fields_config.yaml
         )
     else()
         file( CREATE_LINK
-            ${CMAKE_CURRENT_SOURCE_DIR}/${fypp_file}.F90
-            ${CMAKE_CURRENT_BINARY_DIR}/${fypp_file}.F90
+            ${file_path}.F90
+            ${CMAKE_CURRENT_BINARY_DIR}/${file_name}.F90
             SYMBOLIC COPY_ON_ERROR
         )
     endif()
